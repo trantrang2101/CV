cbe8734bc2d755b536a8c2332dc35caa4ba39999
trantrang2101 <hanhuyettu.cool@gmail.com>
2022-07-14 01:11:47 +0700

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,5 @@
 (function () {
   "use strict";
-
-  // ======= Sticky
-  window.onscroll = function () {
-    const ud_header = document.querySelector(".ud-header");
-    const sticky = ud_header.offsetTop;
-    const logo = document.querySelector(".header-logo");
-
-    if (window.pageYOffset > sticky) {
-      ud_header.classList.add("sticky");
-    } else {
-      ud_header.classList.remove("sticky");
-    }
-
-    // === logo change
-    if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo.svg";
-    } else {
-      logo.src = "assets/images/logo/logo-white.svg";
-    }
-
-    // show or hide the back-top-top button
-    const backToTop = document.querySelector(".back-to-top");
-    if (
-      document.body.scrollTop > 50 ||
-      document.documentElement.scrollTop > 50
-    ) {
-      backToTop.style.display = "flex";
-    } else {
-      backToTop.style.display = "none";
-    }
-  };
-
-  // ===== Sub-menu
-  const submenuItems = document.querySelectorAll(".submenu-item");
-  submenuItems.forEach((el) => {
-    el.querySelector("a").addEventListener("click", () => {
-      el.querySelector(".submenu").classList.toggle("hidden");
-    });
-  });
-
   // ===== Faq accordion
   const faqs = document.querySelectorAll(".single-faq");
   faqs.forEach((el) => {
